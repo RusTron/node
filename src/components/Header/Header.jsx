2df0940c941378d020e7267202e5e5a8d3aa751e
RusTron <rusan.tron@gmail.com>
2020-12-23 02:01:42 +0200

    test

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+export const Header = () => (
+  <header className="App-header">
+    <span
+      className="App-title"
+    >
+      Weather Forecast
+    </span>
+  </header>
+);
